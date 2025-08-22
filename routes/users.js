9795d776898b1172d7503e4ee274a8c2d3b84839
Jade <jadet480@googlemail.com>
2025-08-22 07:59:41 +0100

    Update new user checks

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,12 @@ const { hashPassword, comparePasswords } = require("../helpers/helpers");
 
 /* Register a new user */
 router.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, name, phone, health_notes } = req.body;
+
+  // ensure all required fields are filled out
+  if (!email || !password || !name || !phone) {
+    return res.status(400).json({ message: "Email, password, name, and phone are required" });
+  }
 
   try {
     // check if a user already exists
@@ -21,15 +26,23 @@ router.post("/register", async (req, res) => {
         message: "User already exists",
         next: "/users/login"
       })
-    };
+    }
+
+    // ensure phone does not already exist
+    const phoneCheck = await pool.query(
+      "SELECT * FROM users WHERE phone = $1", 
+      [phone]);
+    if (phoneCheck.rows[0]) {
+      return res.status(400).json({ message: "Phone number already is use" });
+    }
 
     // hash the password 
     const password_hash = await hashPassword(password);
 
     // insert the new user into the db
     const insertNewUser = await pool.query(
-      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING user_id, email, created_at",
-      [email, password_hash]
+      "INSERT INTO users (email, password_hash, name, phone, health_notes) VALUES ($1, $2, $3, $4, $5) RETURNING user_id, email, name, phone, created_at",
+      [email, password_hash, name, phone, health_notes || null]
     );
     const newUser = insertNewUser.rows[0];
 
@@ -41,7 +54,7 @@ router.post("/register", async (req, res) => {
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({message: "Internal server error"});
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
@@ -78,13 +91,16 @@ router.post("/login", async (req, res) => {
       message: "Login successful",
       user: {
         id: user.user_id,
+        name: user.name,
         email: user.email,
+        phone: user.phone,
+        health_notes: user.health_notes,
         created_at: user.created_at
       }
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({message: "Internal server error"});
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
